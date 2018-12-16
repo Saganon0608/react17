aba22d06e58cf5d722cde80423176e89ed1717ab
杨城 <saganon0608@hotmail.com>
2018-12-17 04:11:47 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Button } from 'antd';
+//import './App.css';
+import Admin from './Admin'
+
+class App extends Component {
+  render() {
+    return (
+      <Admin ></Admin>
+    );
+  }
+}
+
+export default App;
